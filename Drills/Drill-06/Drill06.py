b4369b83413e0c1b509978b8db35799acb4b91cc
JahyoKim <kjahyo11@gmail.com>
2018-09-20 19:03:52 +0900

    마우스찍는 위치에따라 캐릭터이동

--- Drills/Drill-06/Drill06.py
+++ Drills/Drill-06/Drill06.py
@@ -6,27 +6,34 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 def handle_events():
     global running
     global x, y
+    global mouse_x, mouse_y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
             x, y = event.x, KPU_HEIGHT - 1 - event.y
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = event.x - 25, KPU_HEIGHT - 1 + 25 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
+hand_arrow = load_image('hand_arrow.png')
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+mouse_x, mouse_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    hand_arrow.draw(x, y)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, mouse_x, mouse_y)
     update_canvas()
     frame = (frame + 1) % 8
 
