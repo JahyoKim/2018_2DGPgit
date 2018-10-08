f37f8acd8b5da525a997b6106111cf08def65f7c
JahyoKim <kjahyo11@gmail.com>
2018-10-09 03:16:43 +0900

    background

--- CookieRun/Background.py
+++ CookieRun/Background.py
@@ -4,8 +4,13 @@ class Background:
     def __init__(self):
         self.background = load_image('background.png')
 
-    def update(self):
-        pass
-
     def draw(self):
-        pass
+        x = int(self.left)
+        w = min(Self.image.w - x, self.screen_width)
+        self.image.clip_draw_to_origin(x, 0, w, self.screen_height,0,0)
+        self.image.clip_draw_to_origin(0, 0, self.screen_width-w, self.screen_height,w,0)
+
+    def update(self,frame_time):
+        self.left = (self.left + frame_time * self.speed) % self.image.w
+
+    
