112ad5b07600318f8c757b2448c4382202f181b8
JahyoKim <kjahyo11@gmail.com>
2018-10-12 01:19:43 +0900

    character

--- CookieRun/Character.py
+++ CookieRun/Character.py
@@ -1,8 +1,21 @@
 from pico2d import *
 
 class Jungle:
+    image_init = None
+
     def __init__(self):
-        pass
+        self.x = 100
+        self.y = 200
+        self.frame = 0
+        self.frame_2 = 0
+        self.state = "run"
+        self.jump_state = "up"
+        self.slide_state = "slide"
+
+        if self.image_init == None:
+            self.run = load_image('jungle_run.png')
+            self.jump = load_image('jungle_jump.png')
+            self.slide = load_image('jungle_slide.png')
 
     def update(self):
         pass
