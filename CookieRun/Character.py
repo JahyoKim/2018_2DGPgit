ba6ba4ac3470c6a5cf81cf6d5b7739cccfc2aaa2
JahyoKim <kjahyo11@gmail.com>
2018-10-13 01:20:57 +0900

    move

--- CookieRun/Character.py
+++ CookieRun/Character.py
@@ -18,10 +18,37 @@ class Jungle:
             self.slide = load_image('jungle_slide.png')
 
     def update(self):
-        pass
+        self.frame +=1
+        if self.frame == 4:
+            self.frame = 0
+        self.frame_2 +=1
+        if self.frame_2 == 2:
+            self.frame_2 = 0
+
+        if self.state == "jump" and self.jump_state == "up":
+            if self.y >= 260:
+                self.jump_state = "down"
+            self.y += 10
+
+        if self.state == "jump" and self.jump_state == "down":
+            if self.y >= 200:
+                self.y -= 10
+
+        if self.state == "jump" and self.y == 200:
+            self.state = "run"
+            self.jump_state = "up"
+
+        if self.state == "slide":
+            self.y = 190
+
 
     def draw(self):
-        pass
+        if self.state == "run":
+            self.run.clip_draw(self.frame * 100, 0, 100, 62, self.x, self.y)
+        elif self.state == "jump":
+            self.jump.clip_draw(self.frame_2 * 90, 0, 90, 63, self.x, self.y)
+        elif self.state == "slide":
+            self.slide.clip_draw(self.frame_2 * 97, 0, 96, 40, self.x, self.y)
 
 open_canvas()
 jungle = Jungle()
