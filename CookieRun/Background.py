d0611f8d874814aa0ea1dc3ddbb53ec5b0779bb1
JahyoKim <kjahyo11@gmail.com>
2018-11-12 23:40:08 +0900

    스크롤링

--- CookieRun/Background.py
+++ CookieRun/Background.py
@@ -10,6 +10,8 @@ class Background:
     SCROLL_SPEED_PPS = (SCROLL_SPEED_MPS * PIXEL_PER_METER)
 
     def __init__(self):
+        self.stage1_x = 400
+        self.stage1_y = 400
         self.frame = 1
         self.speed = 5
         self.distance = 0
@@ -17,8 +19,7 @@ class Background:
 
         if Background.image_init == None:
             self.stage1 = load_image('sprite\\background.png')
-            self.stage2 = load_image('sprite\\background.png')
-            self.stage3 = load_image('sprite\\background.png')
+
             self.bgm = load_music('Sound\\stage1.mp3')
             self.bgm.set_volume(64)
             self.bgm.repeat_play()
@@ -29,14 +30,22 @@ class Background:
 
 
     def draw(self):
-        x = int(self.left)
-        w = min(self.image.w - x, self.screen_width)
-        self.image.clip_draw_to_origin(x, 0, w, self.screen_height, 0, 0)
-        self.image.clip_draw_to_origin(0, 0, self.screen_width-w, self.screen_height, w, 0)
+        if self.count >= 7:
+            self.ChangeState_sound.play()
+            self.stage3.clip_draw(self.frame * 800, 0, 800, 800, self.stage1_x, self.stage1_y)
+            delay(0.05)
+        else:
+            self.stage1.draw(self.stage1_x, self.stage1_y)
 
-    def update(self, frame_time):
-        self.left = (self.left + frame_time * self.speed) % self.image.w
-        self.speed = 100
+
+
+    def update(self,frame_time):
+        if Background.RUN_SPEED_PPS * frame_time > 7:
+            self.distance = 10
+        else:
+            self.distance = Background.RUN_SPEED_PPS * frame_time
+
+
 
 #class Background:
 
